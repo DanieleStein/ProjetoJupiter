e894b62d903eb05112f873727bbdabd0596160a9
DanieleStein <danieleastein@gmail.com>
2023-01-17 09:33:47 -0300

    Transformada a classe Criador Login para Kotlin

--- src/main/java/br/com/projetojupiter/controller/CriadorController.java
+++ src/main/java/br/com/projetojupiter/controller/CriadorController.java
@@ -3,6 +3,7 @@ package br.com.projetojupiter.controller;
 import java.util.Optional;
 
 import br.com.projetojupiter.model.Criador;
+import br.com.projetojupiter.model.CriadorLogin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.projetojupiter.model.CriadorLogin;
 import br.com.projetojupiter.repository.CriadorRepository;
 import br.com.projetojupiter.service.CriadorService;
 
