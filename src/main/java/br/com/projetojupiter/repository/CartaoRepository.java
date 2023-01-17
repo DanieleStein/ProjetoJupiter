cf4450a54fddb0ee4947c4800f61909373c1defc
DanieleStein <danieleastein@gmail.com>
2023-01-17 14:17:42 -0300

    Transformada a classe Cartao para Kotlin

--- src/main/java/br/com/projetojupiter/repository/CartaoRepository.java
+++ src/main/java/br/com/projetojupiter/repository/CartaoRepository.java
@@ -1,9 +1,8 @@
 package br.com.projetojupiter.repository;
 
+import br.com.projetojupiter.model.Cartao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import br.com.projetojupiter.model.Cartao;
 
 @Repository
 public interface CartaoRepository extends JpaRepository<Cartao, Long>{
