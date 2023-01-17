7b5b354a3d752fc7cf6d8bf062dd1d924235c344
DanieleStein <danieleastein@gmail.com>
2023-01-16 23:54:32 -0300

    Transformada a classe Conteudo para Kotlin

--- src/main/java/br/com/projetojupiter/repository/ConteudoRepository.java
+++ src/main/java/br/com/projetojupiter/repository/ConteudoRepository.java
@@ -2,10 +2,9 @@ package br.com.projetojupiter.repository;
 
 import java.util.List;
 
+import br.com.projetojupiter.model.Conteudo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import br.com.projetojupiter.model.Conteudo;
 
 @Repository
 public interface ConteudoRepository extends JpaRepository <Conteudo, Long> {
